009f0ee2719900d2448fb48e2930e44e6d241d7f
Jake <jgoudiehargreaves@gmail.com>
2022-12-14 23:43:45 +0000

    14/12/2022 learning

---
+++ Learning 4/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb2d;
+    [SerializeField] CapsuleCollider2D capsulecollider2d;
+    [SerializeField] BoxCollider2D boxcollider2d;
+    [SerializeField] Animator animator;
+
+    [SerializeField] float moveSpeed = 5;
+    [SerializeField] float jumpHeight = 5;
+    [SerializeField] float climbSpeed = 5;
+    [SerializeField] float coyoteTime = 0.2f;
+    [SerializeField] float jumpBufferTime = 0.2f;
+    
+    float gravityScaleAtStart;
+    float coyoteTimer;
+    float jumpBufferTimer;
+    float moveInput;
+
+    bool isGrounded;
+
+
+    void Start()
+    {
+        gravityScaleAtStart = rb2d.gravityScale;
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+        ClimbLadder();
+    }
+
+    void Move(){
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(horizontal * moveSpeed, rb2d.velocity.y);
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerHasHorizontalSpeed);
+        if(playerHasHorizontalSpeed){
+            transform.localScale = new Vector2 (Mathf.Sign(rb2d.velocity.x), 1f);
+        }
+    }
+
+    void Jump() {
+        isGrounded = boxcollider2d.IsTouchingLayers(LayerMask.GetMask("Ground"));
+
+        if(isGrounded){
+            coyoteTimer = coyoteTime;
+        }
+        else {
+            coyoteTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump")){
+            jumpBufferTimer = jumpBufferTime;
+        }
+        else{ 
+            jumpBufferTimer -= Time.deltaTime;
+        }
+
+        if (jumpBufferTimer >= 0f && coyoteTimer > 0f){
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
+            jumpBufferTimer = 0f;
+        }
+
+        if(Input.GetButtonUp("Jump") && rb2d.velocity.y > 0f)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, rb2d.velocity.y * 0.5f);
+            coyoteTimer = 0;
+        }
+    }
+
+    void FlipSprite(){
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;
+        if(playerHasHorizontalSpeed){
+            transform.localScale = new Vector2 (Mathf.Sign(rb2d.velocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder(){
+        float vertical = Input.GetAxisRaw("Vertical");
+        if(boxcollider2d.IsTouchingLayers(LayerMask.GetMask("Climbing"))){
+            animator.SetBool("isClimbing", true);
+            rb2d.gravityScale = 0;
+            rb2d.velocity = new Vector2(rb2d.velocity.x, vertical * climbSpeed);
+            bool playerHasVerticalSpeed = Mathf.Abs(rb2d.velocity.y) > Mathf.Epsilon;
+            animator.SetBool("isClimbing", playerHasVerticalSpeed);
+        }
+        else{
+            rb2d.gravityScale = gravityScaleAtStart;
+        }
+    }
+}
